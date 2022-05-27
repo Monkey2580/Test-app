318419d6183d1727c5904cbd9c59266ab761d74f
George Marcell <georgemarcell5@gmail.com>
2022-05-27 09:01:45 +0700

    Test git vs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Just Learn React
+          Just Learn!
         </a>
       </header>
     </div>
